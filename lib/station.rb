9f6d46c2783706a6fd41a84393df924b9bd27b73
CodeKrakken <newholm1@hotmail.com>
2020-01-29 15:20:16 +0000

    Station knows its name

--- lib/station.rb
+++ lib/station.rb
@@ -1,2 +1,10 @@
 class Station
+
+  attr_reader :name
+
+  def initialize(name, zone)
+    @name = name
+    @zone = zone
+  end
+
 end
--- spec/station_spec.rb
+++ spec/station_spec.rb
@@ -1,1 +1,11 @@
 require 'station'
+
+describe Station do
+  
+  subject {described_class.new('Brockley', 2)}
+
+  it 'knows its name' do
+    expect(subject.name).to eq('Brockley')
+  end
+
+end
