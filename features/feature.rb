e1ebe6c51a15fa00948516efa299e68a72caf940
CodeKrakken <newholm1@hotmail.com>
2020-01-29 14:11:44 +0000

    Refactor feature test

--- features/feature.rb
+++ features/feature.rb
@@ -1,11 +1,11 @@
 require './lib/oystercard.rb'
 oystercard = Oystercard.new
 oystercard
-oystercard.touch_in
+oystercard.touch_in("Brockley")
 oystercard
 oystercard.top_up(Oystercard::MAXIMUM_BALANCE)
 oystercard.top_up(1)
-oystercard.touch_in
+oystercard.touch_in("Brockley")
 oystercard
 oystercard.touch_out
 oystercard
