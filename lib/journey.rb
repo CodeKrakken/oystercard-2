946492db690b513a66ed25e304948b6c9de43af5
CodeKrakken <newholm1@hotmail.com>
2020-02-22 17:39:22 +0000

    Refactor Journeyb class

--- lib/journey.rb
+++ lib/journey.rb
@@ -12,14 +12,6 @@ class Journey
     @entry_station = station
   end
 
-  def complete?
-    @entry_station.nil? == @exit_station.nil?
-  end
-
-  def fare
-    complete? ? (entry_station.zone.abs - exit_station.zone).abs + 1 : PENALTY_FARE
-  end
-
   def finish(station)
     @exit_station = station
     self
@@ -31,6 +23,14 @@ class Journey
 
   def exit_station
     @exit_station
+  end
+
+  def complete?
+    @entry_station.nil? == @exit_station.nil?
+  end
+
+  def fare
+    complete? ? (entry_station.zone.abs - exit_station.zone).abs + 1 : PENALTY_FARE
   end
 
 end
