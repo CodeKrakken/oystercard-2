2da27531f1fe1739d8b86eb4ef91c18c1b2a7ddf
CodeKrakken <newholm1@hotmail.com>
2020-02-22 18:05:02 +0000

    Refactor oystercard class

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -3,9 +3,7 @@ class Oystercard
   MAXIMUM_BALANCE = 90
   MINIMUM_BALANCE = 1
 
-  attr_reader :balance
-  attr_reader :journey_log
-  attr_reader :journey
+  attr_reader :balance, :journey_log, :journey
 
   def initialize(journey_log_class, journey_class)
     @balance = 0
